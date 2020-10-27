bdad018de276edff99121f4684c89f88ff0fc30f
Su-Kyung <slloph720@naver.com>
2020-10-28 08:33:36 +0900

    basis of the first game (target)

---
+++ IM_midterm/Assets/Scripts/First Game/g1_BulletCtrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class g1_BulletCtrl : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponentInChildren<ParticleSystem>().Play();
+
+        Destroy(gameObject, 2);
+    }
+}
---
+++ IM_midterm/Assets/Scripts/First Game/g1_Shooter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class g1_Shooter : MonoBehaviour
+{
+    public GameObject bullet;
+    public AudioClip sound;
+
+    Vector3 beginPos;
+    AudioSource ads;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ads = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))    // 마우스 누르는 순간 한번만 호출
+        {
+            beginPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            ads.PlayOneShot(sound);
+
+            Vector3 delta = Input.mousePosition - beginPos;
+            Vector3 force = new Vector3(delta.x, delta.y, Mathf.Abs(delta.y));
+
+            GameObject obj = Instantiate(bullet);
+            obj.GetComponent<Rigidbody>().AddForce(force * 5);
+        }
+    }
+}
